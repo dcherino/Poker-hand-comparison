acc28401ed6eb63c355f3f8055183f091adfdbd9
Daniel Cherino <daniel@cherino.me>
2021-02-14 03:31:19 +0000

    Updating comments

--- src/methods/checkLowStraight.ts
+++ src/methods/checkLowStraight.ts
@@ -1,4 +1,4 @@
-// Method to check if the hand is a low straight
+// Method to check if the hand is a low straight (Using Ace as number 1)
 export default function checkLowStraight(faces: Array<string>): Boolean {
   const lowStraightValues = ["A", "2", "3", "4", "5"];
   const uniqFaces = Array.from(new Set(faces));
--- src/methods/groupByValue.ts
+++ src/methods/groupByValue.ts
@@ -1,9 +1,11 @@
 import { CARD_VALUES } from "../constants";
 
-// Checking the concurrences of each hand faces and
-// sorting them so we can check the hand
+// Counting the concurrences of each number (face), store them in an Array
+// and sorting them descendently so we can check the hand.
+//
 // Example for faces: A 4 5 8 A will return:
-// Array<number>[2, 1, 1, 1]  (Two Aces, one 4, one 5 and one 8)
+// Array<number>[2, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+// (Two Aces, one 4, one 5, one 8, zero for the rest of values that weren't in the Array)
 export default function groupByValue(faces: Array<string>): Array<number> {
   const groupedValues = CARD_VALUES.map(
     (cardValue) => faces.filter((value) => value === cardValue).length
