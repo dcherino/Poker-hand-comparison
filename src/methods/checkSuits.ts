2ca8da196a98579ed602d351cb47285fabe90de0
Daniel Cherino <daniel@cherino.me>
2021-02-14 03:31:40 +0000

    Updating SUITS type

--- src/methods/checkSuits.ts
+++ src/methods/checkSuits.ts
@@ -1,5 +1,8 @@
 // Check if all suits are the same in order to consider
+
+import { Suits } from "../types";
+
 // the hand as Flush or Straight flush
-export default function checkSuits(suits: Array<string>): Boolean {
+export default function checkSuits(suits: Array<Suits>): Boolean {
   return suits.every((suit) => suit === suits[0]);
 }
